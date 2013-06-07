9680d6bc4984604b8646c11c8f95e1c45f83761f
k1000 <selwak@gmail.com>
2013-06-07 07:47:03 +0200

    more digits in tel

--- evreg/models.py
+++ evreg/models.py
@@ -225,7 +225,7 @@ class Registry(models.Model):
     last_name = models.CharField(_("last name"), max_length=50)
 
     email = models.EmailField(_("email"))
-    phone = models.CharField(_("phone"), max_length=12,
+    phone = models.CharField(_("phone"), max_length=20,
         blank=True, null=True,)
 
     address = models.TextField(_("address"), max_length=250)
