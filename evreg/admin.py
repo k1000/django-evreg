142d637e4bf4c5aa47a07992d6fb00b14d155951
k1000 <selwak@gmail.com>
2013-06-25 14:14:55 +0200

    corrections admin

--- evreg/admin.py
+++ evreg/admin.py
@@ -21,16 +21,30 @@ class RegistryAdmin(CSVExportableAdmin):
                 'event',
                 ('first_name', 'last_name'),
                 'status',
+                'observations')
+        }),
+        (_('Contact'),
+            {
+            'fields': (
                 ('email', 'phone',),
                 'address',
                 ('postal_code', "city",),
                 'country',
-                ('member_type', 'membership_nr', 'gar'),
-                # 'karmayoga',
-                ('payment_time', 'payment_amount', 'payment_id')),
+                ),
         }),
+        (_('Membership'),
+            {
+            'fields': (('member_type', 'member_validated',)
+                        ('gar', 'membership_nr'),
+                )}
+        ),
+        (_('Payment'),
+            {
+            'fields': (('payment_time', 'payment_amount', 'payment_id'),
+                )}
+        ),
     )
-    list_display = ['first_name', 'last_name', 'id', 'event', 'status', 'member_type']
+    list_display = ['first_name', 'last_name', 'id', 'event', 'status', 'member_type', 'member_validated']
     csv_list_display = list_display + ["membership_nr", "gar", "email", "payment_amount"]
     list_filter = ['event', 'gar', 'status', 'member_type', ]
     search_fields = ['last_name', 'email', 'membership_nr']
@@ -69,7 +83,7 @@ class EventAdmin(admin.ModelAdmin):
                 ('name', 'slug'),
                 ('start', 'finish'),
                 'description',
-                'organizer',
+                ('organizer', 'contact_tel'),
                 ('contact_email', 'web'),
                 ('venue_name', 'venue_address'),
                 ('registration_until', 'earlybird_date',),
